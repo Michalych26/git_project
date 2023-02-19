fbf4588262dda0e7d38127b58c7386bf065eb985
Michalych26 <65976708+Michalych26@users.noreply.github.com>
2023-02-20 00:13:09 +0300

    функция сумм 2 чисел

--- py_project/funcs.py
+++ py_project/funcs.py
@@ -1,5 +1,12 @@
-def sum_numbers():
-    a=10
-    b=20
-    print(a+b)
+def sum_numbers(a, b):
+    print(a + b)
     return
+
+
+def number_subtraction(a, b):
+    print(a - b)
+    return
+
+
+#um_numbers(10, 50)
+#number_subtraction(50, 10)
